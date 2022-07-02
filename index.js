53752e82bc1422f18f9cceb989ac89ef2c464b81
Felix Moser <f.moser@tum.de>
2022-07-02 14:41:59 +0200

    first try

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.post('/', (req, res) => {
-    res.send('Hello World!')
-})
+var hana = require('@sap/hana-client');
 
-app.listen(process.env.PORT || 3000,
-    () => console.log("Server is running..."));
+var conn = hana.createConnection();
+
+var conn_params = {
+    serverNode: 'f10e1026-9095-43c1-a969-d649862a1cd6.hana.trial-us10.hanacloud.ondemand.com:443',
+    encrypt: 'true',
+    sslValidateCertificate: 'false',
+    uid: 'DBADMIN',
+    pwd: 'Asdfghjkl345'
+};
+
+// open DB connection
+conn.connect(conn_params, function (err) {
+    if (err) throw err;
+    console.log("Connection established");
+
+    // Put listener for Photoelectric barrier here
+
+    app.post('/increment', (req, res) => {
+        conn.exec('UPDATE Joe SET counter = counter + 1 WHERE id = ?', [1], function (err, result) {
+            if (err) { req.send(err) };
+            console.log("Increment:", result);
+            req.send(result)
+        })
+    })
+
+
+    app.post('/decrement', (req, res) => {
+        conn.exec('UPDATE Joe SET counter = counter - 1 WHERE id = ?', [1], function (err, result) {
+            if (err) { req.send(err) };
+            console.log("Increment:", result);
+            req.send(result)
+        })
+    })
+
+
+    app.get('/data', (req, res) => {
+        conn.exec('SELECT * FROM Joe WHERE id = ?', [1], function (err, result) {
+            if (err) { req.send(err) }
+            else { req.send(result) }
+        })
+    })
+
+
+    app.listen(process.env.PORT || 3000,
+        () => console.log("Server is running..."));
+});
+
+
+app.close(() => {
+    conn.disconnect();
+    console.log('Closed out remaining connections');
+    process.exit(0);
+});
